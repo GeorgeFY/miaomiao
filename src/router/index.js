47893b9f7cdd84a84c808fbf35608f7eaf4618c4
GeorgeYF <466912902@qq.com>
2020-11-21 07:03:53 +0800

    comments create add

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
-
+import cinameRouter from "./cinema";
+import mineRouter from "./mine";
+import movieRouter from "./movie";
 Vue.use(VueRouter);
 
 const routes = [
+  cinameRouter,
+  mineRouter,
+  movieRouter,
   {
-    path: "/",
-    name: "Home",
-    component: Home
-  },
-  {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: "/*",
+    redirect: "/movie"
   }
 ];
 
---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="main">
+    <Header title="喵喵影院"></Header>
+	<div id="content">
+		<CinemaHeader></CinemaHeader>
+		<keep-alive>
+			<CinemaList></CinemaList>
+		</keep-alive>
+		
+	</div>
+    <TabBar></TabBar>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import TabBar from "@/components/TabBar";
+import CinemaHeader from "@/components/CinemaHeader";
+import CinemaList from "@/components/CinemaList";
+export default {
+  name: "Cinema",
+  components: {
+    Header,
+    TabBar,
+	CinemaHeader,
+	CinemaList
+  }
+};
+</script>
+
+<style></style>
